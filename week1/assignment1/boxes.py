81bb00e1d9ed7c1bfd1681c80f2e8711dc29a244
Johnathon D Smith <jd49490@me.com>
2025-07-01 11:25:40 -0400

    Fixes type error in box calculation
    Ensures that the input values for totalItems and itemsPerBox are converted to floats before division.
    
    This prevents potential type errors and ensures accurate calculation of the number of boxes required.

--- week1/assignment1/boxes.py
+++ week1/assignment1/boxes.py
@@ -3,6 +3,6 @@ import math
 totalItems = input("Enter the number of items:")
 itemsPerBox = input("Enter the number of items per box:")
 
-boxes = math.ciel(totalItems / itemsPerBox)
+boxes = math.ceil(float(totalItems) / float(itemsPerBox))
 
 print(f"For {totalItems} items, packing {itemsPerBox} items in each box, you will need {boxes} boxes.")
