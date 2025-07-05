9da7002b5ed84be976dbfd7a34b5a6208b28f741
Johnathon Smith <jsmith@autosoftdms.com>
2025-07-04 23:56:00 -0400

    Adds tire volume calculator application
    Implements a new application for calculating tire volume.
    
    The application enhances user experience by leveraging additional libraries.
    Installation instructions for required libraries are included in the code comments.
    It requires Python 3.10 or higher to run properly.

--- week1/assignment3/tv_app/tire_volume.py
+++ week1/assignment3/tv_app/tire_volume.py
@@ -1,3 +1,9 @@
+## I choose to implement a tire volume calculator as a simple application that
+# uses some additional libraries to enhance the user experience.  When running
+# this code, make sure you have the required libraries installed:
+# First, rename the requirements file `req.txt.py` to `req.txt` and then run:
+# pip install -r req.txt
+## The application requires python 3.10 or higher.
 from App import App
 
 app = App("Tire Volume Calculator")
